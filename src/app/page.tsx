d4b2acb4ba7d02ab9ff26f8eb30ae50463abd740
neoenginex <neoenginex@gmail.com>
2025-10-15 20:03:51 -0600

    Database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-const Page = () => {
+import prisma from "@/lib/db"
+
+const Page = async () => {
+  const users = await prisma.user.findMany();
+
   return (
-    <div>
-      Hello World
+    <div className="min-h-screen min-w-screen flex items-center justify-center">
+      {JSON.stringify(users)}
     </div>
   )
 }
-
 export default Page;
