8e3e707d647efd0d2dfc1417b34a063aed1ef78c
neoenginex <neoenginex@gmail.com>
2025-10-16 19:01:08 -0600

    Background Jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,33 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
 import { requireAuth } from "@/lib/auth-utils";
 import { caller } from "@/trpc/server";
 import { LogoutButton } from "./logout";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useTRPC } from "@/trpc/client"
+import { toast } from "sonner";
 
-const Page = async () => {
-  await requireAuth();
+const Page = () => {
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
+  const queryClient = useQueryClient(); 
 
-  const data = await caller.getUsers();
+  const create = useMutation(trpc.createWorkflow.mutationOptions({
+    onSuccess: () => {
+      toast.success("Job Queued")
+    }
+  }));
 
   return (
-    <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gay-y-6">
+    <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
       Protected Server Component:
       <div>
         {JSON.stringify(data, null, 2)}
       </div>
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create Workflow
+      </Button>
       <LogoutButton />
     </div>
   );
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,13 +1,21 @@
-import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
-import prisma from "@/lib/db"
+import { createTRPCRouter, protectedProcedure } from '../init';
+import prisma from "@/lib/db";
+import { inngest } from "@/inngest/client";
+
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
-      return prisma.user.findMany({
-        where: {
-          id: ctx.auth.user.id,
-        }
-      });
-    }),
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
+    return prisma.workflow.findMany();
+  }),
+  createWorkflow: protectedProcedure.mutation(async () => {
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: "wilson.asher00@gmail.com",
+      }
+    }) 
+
+    return { success: true, message: "Job Queued" }
+  }),
 });
-// export type definition of API
+
 export type AppRouter = typeof appRouter;
