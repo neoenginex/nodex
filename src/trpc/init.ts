ffe4dda8ec48b9bd4d80afabe10c274c79ffa341
neoenginex <neoenginex@gmail.com>
2025-10-17 03:20:40 -0600

    Added Polar

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -2,6 +2,7 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { initTRPC, TRPCError } from '@trpc/server';
 import { cache } from 'react';
+import { polarClient } from "@/lib/polar";
 export const createTRPCContext = cache(async () => {
   /**
    * @see: https://trpc.io/docs/server/context
@@ -36,3 +37,22 @@ export const protectedProcedure = baseProcedure.use(async ({ ctx, next }) => {
 
   return next({ ctx: { ...ctx, auth: session } });
 });
+export const premiumProcedure = protectedProcedure.use(
+  async ({ ctx, next }) => {
+    const customer = await polarClient.customers.getStateExternal({
+      externalId: ctx.auth.user.id,
+    });
+
+    if (
+      !customer.activeSubscriptions ||
+      customer.activeSubscriptions.length === 0
+    ) {
+      throw new TRPCError({
+        code: "FORBIDDEN",
+        message: "Active subscription required.",
+      });
+    }
+
+    return next({ ctx: { ...ctx, customer } });
+  },
+);
