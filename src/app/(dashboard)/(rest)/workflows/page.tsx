a2a6f7baad7b3065c58fe61cd8e9d24d6a76900c
neoenginex <neoenginex@gmail.com>
2025-10-17 16:31:11 -0600

    Workflows CRUD

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,9 +1,26 @@
+import { prefetchWorkflows } from "@/features/workflows/server/prefetch";
 import { requireAuth } from "@/lib/auth-utils";
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense } from "react";
+import { HydrateClient } from "@/trpc/server";
+import { WorkflowsContainer, WorkflowsList } from "@/features/workflows/components/workflows";
 
 const Page = async () => {
     await requireAuth();
 
-    return <p>Workflows</p>
+    prefetchWorkflows();
+
+    return (
+        <WorkflowsContainer>
+         <HydrateClient>
+           <ErrorBoundary fallback={<p>Error!</p>}>
+              <Suspense fallback={<p>Loading...</p>}>
+                <WorkflowsList />
+              </Suspense>
+            </ErrorBoundary>
+          </HydrateClient>
+        </WorkflowsContainer>
+    )
 };
 
 export default Page;
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,28 +1,8 @@
-import { baseProcedure, createTRPCRouter, premiumProcedure, protectedProcedure } from '../init';
-import prisma from "@/lib/db";
-import { inngest } from "@/inngest/client";
+import { createTRPCRouter } from '../init';
+import { workflowsRouter } from "@/features/workflows/server/routers";
 
 export const appRouter = createTRPCRouter({
-  testAi: premiumProcedure.mutation(async () => {
-    await inngest.send({
-      name: "execute/ai",
-    });
-
-    return { success: true, message: "Job Queued" }
-  }),
-  getWorkflows: protectedProcedure.query(({ ctx }) => {
-    return prisma.workflow.findMany();
-  }),
-  createWorkflow: protectedProcedure.mutation(async () => {
-    await inngest.send({
-      name: "test/hello.world",
-      data: {
-        email: "wilson.asher00@gmail.com",
-      }
-    }) 
-
-    return { success: true, message: "Job Queued" }
-  }),
+  workflows: workflowsRouter,
 });
 
 export type AppRouter = typeof appRouter;
